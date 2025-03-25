0f8f249bd01ef9e1582861f198c1110025e7d2a8
Lukas Kral <lukywill16@gmail.com>
2025-03-25 11:05:39 +0100

    Change version in TestUtils
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- src/test/java/io/strimzi/kafka/metrics/TestUtils.java
+++ src/test/java/io/strimzi/kafka/metrics/TestUtils.java
@@ -44,7 +44,7 @@ import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
 @SuppressWarnings("ClassFanOutComplexity")
 public class TestUtils {
 
-    private static final String VERSION = "1.0.0-SNAPSHOT";
+    private static final String VERSION = "0.2.0";
     private static final String KAFKA_VERSION = "3.9.0";
     private static final String CLIENTS_IMAGE = "quay.io/strimzi-test-clients/test-clients:latest-kafka-" + KAFKA_VERSION;
     private static final Duration TIMEOUT = Duration.ofSeconds(10L);
