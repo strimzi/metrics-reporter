0e03d43d123e78d1161fef85ac40acc238db4945
Mickael Maison <mickael.maison@gmail.com>
2025-06-02 17:40:57 +0200

    Bump test timeout
    Signed-off-by: Mickael Maison <mickael.maison@gmail.com>

--- client-metrics-reporter/src/test/java/io/strimzi/kafka/metrics/prometheus/MetricsUtils.java
+++ client-metrics-reporter/src/test/java/io/strimzi/kafka/metrics/prometheus/MetricsUtils.java
@@ -44,7 +44,7 @@ public class MetricsUtils {
 
     public static final String VERSION = "1.0.0-SNAPSHOT";
     private static final String CLIENTS_IMAGE = "quay.io/strimzi-test-clients/test-clients:latest-kafka-3.9.0";
-    private static final Duration TIMEOUT = Duration.ofSeconds(10L);
+    private static final Duration TIMEOUT = Duration.ofSeconds(30L);
 
     public static final String REPORTER_JARS = "target/client-metrics-reporter-" + VERSION + "/client-metrics-reporter-" + VERSION + "/libs/";
     public static final String MOUNT_PATH = "/opt/strimzi/metrics-reporter/";
