c1f03cceccc024ce4a795afcf1d2a9c5d2f1f8a1
Mickael Maison <mimaison@users.noreply.github.com>
2025-07-07 13:05:41 +0200

    Use StrimziKafkaCluster instead of StrimziKafkaContainer (#99)
    Signed-off-by: Mickael Maison <mickael.maison@gmail.com>

--- server-metrics-reporter/src/test/java/io/strimzi/kafka/metrics/prometheus/integration/TestServerMetricsIT.java
+++ server-metrics-reporter/src/test/java/io/strimzi/kafka/metrics/prometheus/integration/TestServerMetricsIT.java
@@ -9,7 +9,7 @@ import io.strimzi.kafka.metrics.prometheus.ServerKafkaMetricsReporter;
 import io.strimzi.kafka.metrics.prometheus.ServerMetricsReporterConfig;
 import io.strimzi.kafka.metrics.prometheus.ServerYammerMetricsReporter;
 import io.strimzi.kafka.metrics.prometheus.http.Listener;
-import io.strimzi.test.container.StrimziKafkaContainer;
+import io.strimzi.test.container.StrimziKafkaCluster;
 import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.AlterConfigOp;
@@ -18,8 +18,8 @@ import org.apache.kafka.clients.admin.ConfigEntry;
 import org.apache.kafka.common.config.ConfigResource;
 import org.apache.kafka.common.errors.InvalidRequestException;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.MountableFile;
 
 import java.util.HashMap;
@@ -38,85 +38,96 @@ public class TestServerMetricsIT {
 
     private static final String REPORTER_JARS = "../target/metrics-reporter-" + VERSION + "/metrics-reporter-" + VERSION + "/libs/";
     private static final int PORT = Listener.parseListener(ServerMetricsReporterConfig.LISTENER_CONFIG_DEFAULT).port;
-    private static final int NODE_ID = 0;
 
-    private Map<String, String> configs;
-    private StrimziKafkaContainer broker;
-
-    @BeforeEach
-    public void setUp() {
-        configs = new HashMap<>();
-        configs.put("metric.reporters", ServerKafkaMetricsReporter.class.getName());
-        configs.put("kafka.metrics.reporters", ServerYammerMetricsReporter.class.getName());
-
-        broker = new StrimziKafkaContainer()
-                .withNodeId(NODE_ID)
-                .withCopyFileToContainer(MountableFile.forHostPath(REPORTER_JARS), MetricsUtils.MOUNT_PATH)
-                .withExposedPorts(9092, PORT)
-                .withKafkaConfigurationMap(configs)
-                .withEnv(Map.of("CLASSPATH", MetricsUtils.MOUNT_PATH + "*"));
-    }
+    private StrimziKafkaCluster cluster;
 
     @AfterEach
     public void tearDown() {
-        broker.stop();
+        if (cluster != null) {
+            cluster.stop();
+        }
+    }
+
+    private void setupCluster(Map<String, String> overrides) {
+        Map<String, String> configs = new HashMap<>(overrides);
+        configs.put("metric.reporters", ServerKafkaMetricsReporter.class.getName());
+        configs.put("kafka.metrics.reporters", ServerYammerMetricsReporter.class.getName());
+
+        cluster = new StrimziKafkaCluster.StrimziKafkaClusterBuilder()
+                .withAdditionalKafkaConfiguration(configs)
+                .withNumberOfBrokers(1)
+                .withSharedNetwork()
+                .build();
+        for (GenericContainer<?> broker : cluster.getNodes()) {
+            broker.withCopyFileToContainer(MountableFile.forHostPath(MetricsUtils.REPORTER_JARS), MetricsUtils.MOUNT_PATH)
+                    .withCopyFileToContainer(MountableFile.forHostPath(REPORTER_JARS), MetricsUtils.MOUNT_PATH)
+                    .withExposedPorts(9092, PORT)
+                    .withEnv(Map.of("CLASSPATH", MetricsUtils.MOUNT_PATH + "*"));
+        }
+        cluster.start();
     }
 
     @Test
     public void testBrokerMetrics() {
-        broker.start();
+        setupCluster(Map.of());
 
         List<String> patterns = List.of(
-            "jvm_.*",
-            "process_.*",
-            "kafka_controller_.*",
-            "kafka_coordinator_.*",
-            "kafka_log_.*",
-            "kafka_network_.*",
-            "kafka_server_.*");
-        MetricsUtils.verify(broker, patterns, PORT, metrics -> assertFalse(metrics.isEmpty()));
+                "jvm_.*",
+                "process_.*",
+                "kafka_controller_.*",
+                "kafka_coordinator_.*",
+                "kafka_log_.*",
+                "kafka_network_.*",
+                "kafka_server_.*");
+        for (GenericContainer<?> broker : cluster.getNodes()) {
+            MetricsUtils.verify(broker, patterns, PORT, metrics -> assertFalse(metrics.isEmpty()));
+        }
     }
 
     @Test
     public void testBrokerMetricsWithAllowlist() {
-        configs.put(ALLOWLIST_CONFIG, "kafka_controller.*,kafka_server.*");
-        broker.withKafkaConfigurationMap(configs);
-        broker.start();
+        setupCluster(Map.of(ALLOWLIST_CONFIG, "kafka_controller.*,kafka_server.*"));
 
         List<String> allowedPatterns = List.of(
             "jvm_.*",
             "process_.*",
             "kafka_controller_.*",
             "kafka_server_.*");
-        MetricsUtils.verify(broker, allowedPatterns, PORT, metrics -> assertFalse(metrics.isEmpty()));
+        for (GenericContainer<?> broker : cluster.getNodes()) {
+            MetricsUtils.verify(broker, allowedPatterns, PORT, metrics -> assertFalse(metrics.isEmpty()));
+        }
 
         List<String> disallowPatterns = List.of(
             "kafka_coordinator_.*",
             "kafka_log_.*",
             "kafka_network_.*");
-        MetricsUtils.verify(broker, disallowPatterns, PORT, metrics -> assertTrue(metrics.isEmpty()));
+        for (GenericContainer<?> broker : cluster.getNodes()) {
+            MetricsUtils.verify(broker, disallowPatterns, PORT, metrics -> assertTrue(metrics.isEmpty()));
+        }
     }
 
     @Test
     public void testReconfigureAllowlist() throws Exception {
-        configs.put(ALLOWLIST_CONFIG, "kafka_controller.*,kafka_server.*");
-        broker.withKafkaConfigurationMap(configs);
-        broker.start();
+        setupCluster(Map.of(ALLOWLIST_CONFIG, "kafka_controller.*,kafka_server.*"));
 
         List<String> allowedPatterns = List.of(
                 "jvm_.*",
                 "process_.*",
                 "kafka_controller_.*",
                 "kafka_server_.*");
-        MetricsUtils.verify(broker, allowedPatterns, PORT, metrics -> assertFalse(metrics.isEmpty()));
+        for (GenericContainer<?> broker : cluster.getNodes()) {
+            MetricsUtils.verify(broker, allowedPatterns, PORT, metrics -> assertFalse(metrics.isEmpty()));
+        }
 
         List<String> disallowPatterns = List.of(
                 "kafka_coordinator_.*",
                 "kafka_log_.*",
                 "kafka_network_.*");
-        MetricsUtils.verify(broker, disallowPatterns, PORT, metrics -> assertTrue(metrics.isEmpty()));
+        for (GenericContainer<?> broker : cluster.getNodes()) {
+            MetricsUtils.verify(broker, disallowPatterns, PORT, metrics -> assertTrue(metrics.isEmpty()));
+        }
 
-        try (Admin admin = Admin.create(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, broker.getBootstrapServers()))) {
+        try (Admin admin = Admin.create(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, cluster.getBootstrapServers()))) {
             admin.incrementalAlterConfigs(Map.of(
                     new ConfigResource(ConfigResource.Type.BROKER, ""),
                     List.of(new AlterConfigOp(
@@ -131,37 +142,41 @@ public class TestServerMetricsIT {
                 "kafka_coordinator_.*",
                 "kafka_log_.*",
                 "kafka_network_.*");
-        MetricsUtils.verify(broker, allowedPatterns, PORT, metrics -> assertFalse(metrics.isEmpty()));
+        for (GenericContainer<?> broker : cluster.getNodes()) {
+            MetricsUtils.verify(broker, allowedPatterns, PORT, metrics -> assertFalse(metrics.isEmpty()));
+        }
 
         disallowPatterns = List.of(
                 "kafka_controller_.*",
                 "kafka_server_.*");
-        MetricsUtils.verify(broker, disallowPatterns, PORT, metrics -> assertTrue(metrics.isEmpty()));
+        for (GenericContainer<?> broker : cluster.getNodes()) {
+            MetricsUtils.verify(broker, disallowPatterns, PORT, metrics -> assertTrue(metrics.isEmpty()));
+        }
     }
 
     @Test
     public void testReconfigureValidatesAllowlist() throws Exception {
-        configs.put(ALLOWLIST_CONFIG, "kafka_controller.*,kafka_server.*");
-        broker.withKafkaConfigurationMap(configs);
-        broker.start();
+        setupCluster(Map.of(ALLOWLIST_CONFIG, "kafka_controller.*,kafka_server.*"));
 
-        try (Admin admin = Admin.create(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, broker.getBootstrapServers()))) {
-            ConfigResource cr = new ConfigResource(ConfigResource.Type.BROKER, String.valueOf(NODE_ID));
-            try {
-                admin.incrementalAlterConfigs(Map.of(
-                        cr,
-                        List.of(new AlterConfigOp(
-                                new ConfigEntry(ALLOWLIST_CONFIG, "not_a_pattern[[("),
-                                AlterConfigOp.OpType.SET))
-                )).all().get();
-            } catch (ExecutionException ee) {
-                assertInstanceOf(InvalidRequestException.class, ee.getCause());
-                assertTrue(ee.getCause().getMessage().contains("ConfigException"));
-                assertTrue(ee.getCause().getMessage().contains("Invalid regex pattern found"));
+        for (int brokerId = 0; brokerId < cluster.getNodes().size(); brokerId++) {
+            try (Admin admin = Admin.create(Map.of(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, cluster.getBootstrapServers()))) {
+                ConfigResource cr = new ConfigResource(ConfigResource.Type.BROKER, String.valueOf(brokerId));
+                try {
+                    admin.incrementalAlterConfigs(Map.of(
+                            cr,
+                            List.of(new AlterConfigOp(
+                                    new ConfigEntry(ALLOWLIST_CONFIG, "not_a_pattern[[("),
+                                    AlterConfigOp.OpType.SET))
+                    )).all().get();
+                } catch (ExecutionException ee) {
+                    assertInstanceOf(InvalidRequestException.class, ee.getCause());
+                    assertTrue(ee.getCause().getMessage().contains("ConfigException"));
+                    assertTrue(ee.getCause().getMessage().contains("Invalid regex pattern found"));
+                }
+
+                Config config = admin.describeConfigs(List.of(cr)).all().get().get(cr);
+                assertEquals(ConfigEntry.ConfigSource.STATIC_BROKER_CONFIG, config.get(ALLOWLIST_CONFIG).source());
             }
-
-            Config config = admin.describeConfigs(List.of(cr)).all().get().get(cr);
-            assertEquals(ConfigEntry.ConfigSource.STATIC_BROKER_CONFIG, config.get(ALLOWLIST_CONFIG).source());
         }
     }
 }
