e0e49e8a626a3cf081c7d9fab793894d3dc7f1ab
Mickael Maison <mickael.maison@gmail.com>
2026-01-28 10:33:45 +0100

    Bump dependencies for new release
    Signed-off-by: Mickael Maison <mickael.maison@gmail.com>

--- client-metrics-reporter/src/test/java/io/strimzi/kafka/metrics/prometheus/MetricsUtils.java
+++ client-metrics-reporter/src/test/java/io/strimzi/kafka/metrics/prometheus/MetricsUtils.java
@@ -43,7 +43,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class MetricsUtils {
 
     public static final String VERSION = "1.0.0-SNAPSHOT";
-    private static final String CLIENTS_IMAGE = "quay.io/strimzi-test-clients/test-clients:latest-kafka-4.0.0";
+    private static final String CLIENTS_IMAGE = "quay.io/strimzi-test-clients/test-clients:0.12.0-kafka-4.0.0";
     private static final Duration TIMEOUT = Duration.ofSeconds(30L);
 
     public static final String REPORTER_JARS = "target/client-metrics-reporter-" + VERSION + "/client-metrics-reporter-" + VERSION + "/libs/";
