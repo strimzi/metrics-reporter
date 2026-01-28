9689aa815fc0d0bd493ed73d572f9657a8089bdc
Mickael Maison <mickael.maison@gmail.com>
2026-01-28 10:51:49 +0100

    Prepare for 0.3.0 release
    Signed-off-by: Mickael Maison <mickael.maison@gmail.com>

--- client-metrics-reporter/src/test/java/io/strimzi/kafka/metrics/prometheus/MetricsUtils.java
+++ client-metrics-reporter/src/test/java/io/strimzi/kafka/metrics/prometheus/MetricsUtils.java
@@ -42,7 +42,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  */
 public class MetricsUtils {
 
-    public static final String VERSION = "1.0.0-SNAPSHOT";
+    public static final String VERSION = "0.3.0";
     private static final String CLIENTS_IMAGE = "quay.io/strimzi-test-clients/test-clients:0.12.0-kafka-4.0.0";
     private static final Duration TIMEOUT = Duration.ofSeconds(30L);
 
